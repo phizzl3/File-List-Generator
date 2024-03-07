b8ee62a1e211b3566c5d987621ba775ba44c0911
Brandon Phillips <phizzl3@gmail.com>
2024-03-07 10:24:30 -0800

    Refactor/update utils (#4)
    * Update/Replace dropfile.py
    
    * Update/Replace openfile.py

--- file-list-generator/__main__.py
+++ file-list-generator/__main__.py
@@ -9,8 +9,8 @@ import time
 from pathlib import Path
 
 import listgen
-from utils import dropfile
-from utils.openfile import openfile
+from utils.dropfile import get_dropped_file
+from utils.openfile import open_file
 
 # Set output folder in user's Home directory and T/F to open output file
 OUTPUT_FOLDER = "Downloads"
@@ -21,7 +21,7 @@ target_folder = Path().home().resolve() / OUTPUT_FOLDER
 
 # Get source filepath
 print("\n Drop the folder you'd like to list the contents of...")
-source_folder = dropfile.get()
+source_folder = get_dropped_file()
 
 # List files and output text file with contents
 output_file = listgen.main(source_folder=source_folder,
@@ -29,7 +29,7 @@ output_file = listgen.main(source_folder=source_folder,
 
 # Optionally open output file or display a message
 if OPEN_FILE:
-    openfile(output_file)
+    open_file(output_file)
 else:
     print(f"\n File saved to {output_file}")
     time.sleep(3)
--- file-list-generator/utils/dropfile.py
+++ file-list-generator/utils/dropfile.py
@@ -1,32 +1,16 @@
-# 01.22.2021
+__version__ = "1.1.1"
 
 from pathlib import Path
 
 
-def get():
-    """
-    Gets file/directory via drag and drop and return a Path object.
+def get_dropped_file() -> Path:
+    file_location = input("\n Drop File: ")
+    file_location = __clean_characters(file_location)
+    return Path(file_location).resolve()
 
-    Asks for file or directory via drag and drop, strips unneeded characters,
-    and uses pathlib.Path to return a Path object.
 
-    Returns:
-    - (pathlib.PosixPath object): A pathlib.PosixPath object that can be 
-    used to get file path, filname, extension, etc. 
-
-    Sample Usage:
-    - p = dropfile.get()
-        - p = full path
-        - p.parent = parent folder
-        - p.stem = filename excluding extension
-        - p.suffix = file extension
-    """
-    
-    # Get input file/folder and strip characters
-    f = input('\n Drop Folder: ')
-    f = f.strip(" &'\"")
-    f = f.replace("\ ", " ")
-    # set Path object and return
-    p = Path(f).resolve()
-    return p
-    
+def __clean_characters(file_location: str) -> str:
+    file_location = file_location.strip(" &'\"")
+    file_location = file_location.replace("\ ", " ")
+    file_location = file_location.replace("''", "'")
+    return file_location
